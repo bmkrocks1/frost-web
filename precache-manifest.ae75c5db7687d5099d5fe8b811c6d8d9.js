8b1b3a7ed912aeed88f74fad43c12ce5d7e3d34b
Billie Ko <bmkrocks@gmail.com>
2020-02-04 21:31:58 +0800

    Updates

---
+++ precache-manifest.ae75c5db7687d5099d5fe8b811c6d8d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa889af1f7a7b83ca9af3a49d11dd72",
+    "url": "/frost-web/index.html"
+  },
+  {
+    "revision": "544efaef8d3e7e67bc8c",
+    "url": "/frost-web/static/js/2.aa9ee01b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/frost-web/static/js/2.aa9ee01b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22000940c44a91107ce1",
+    "url": "/frost-web/static/js/main.d6735bcd.chunk.js"
+  },
+  {
+    "revision": "25c9f25a0d2b679f5ce9",
+    "url": "/frost-web/static/js/runtime-main.3bd3665f.js"
+  }
+]);
